2b94af27fb459f4a2050f91ec8a604f0e1443dd3
Evyatar <evyamadmon@gmail.com>
2024-01-10 20:02:01 +0200

    blabla

--- db/connection.js
+++ db/connection.js
@@ -4,8 +4,8 @@ const mysql = require("mysql2/promise");
 const connection = mysql.createPool({
   host: "localhost",
   user: "root",
-  password: "1234",
-  database: "test",
+  password: "evya246810",
+  database: "charity",
 });
 
 
